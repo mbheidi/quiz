81da63bca8ce23c79ac62ca7bc6fa61e5df3b03a
mbheidi <78099151+mbheidi@users.noreply.github.com>
2021-02-06 18:07:16 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
-
-
-
-
-
+  $("h1").click(function() {
+		$(".className").hide();
+		$("p").css({"color":"red", "background-color": "yellow"});
+		$("img").css("border","25px solid black");
+	});
+	$("#imagetoggler").click(function(){
+		$("img").toggle();
+	});
 });
-                             
+
+
